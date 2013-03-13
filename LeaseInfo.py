c27b6287e3273de89ce4c1bebc9243ca0485485e
jm1024 <james@packrat.org>
2013-03-13 17:41:26 -0400

    Sorts by ip and eliminates dups

--- LeaseInfo.py
+++ LeaseInfo.py
@@ -3,33 +3,40 @@
 def parse(fileName):
 
     f = open( fileName, "r" )
-    output = ""
+    output = {}
+    thisIp = ""
+    thisMAC = ""
+    thisName = ""
     
     for line in f:
         
         # ip
-        if (line.find("lease") > -1):
-            if (output != ""):
-                print(output)
-                output = ""
+        if (line.find("lease") == 0):
+            if (thisIp != ""):
+                output[thisIp] = thisIp + " " + thisMAC + " " + thisName
+                thisMAC = ""
+                thisName = ""
+                
             thisIp = line[line.find(" ") +1 : line.find("{") -1]
-            output = thisIp
-            
+
         # mac
         if(line.find("hardware ethernet") > -1):
             thisMAC = line[line.find("ethernet") +9 : line.find(";")]
-            output += " " + thisMAC
             
         # hostname
         if(line.find("client-hostname") > -1):
             thisName = line[line.find("hostname") +10 : line.find(";") -1]
-            output += " " + thisName 
             
-    if(output != ""):
-        print(output)
+    #final line
+    if (thisIp != ""):
+        output[thisIp] = thisIp + " " + thisMAC + " " + thisName
+
+    #print output
+    for k, v in sorted(output.items()):
+        print(v)
 
     # Close the file
     f.close()
     
 #parse("/home/james/Source/LeaseInfo/dhcpd.leases")
-parse("/var/lib/dhcpd/dhcpd.leases")
+parse("/var/lib/dhcp/dhcpd.leases")
