4eaf2928443c5e009c4fb27e14b9cb3b0d632075
Vaclav Klecanda <vencax77@gmail.com>
2014-06-10 22:19:28 +0200

    setup files

---
+++ README.md
@@ -0,0 +1,1 @@
+Parses ISC dhcpd leases file and returns dict[IP, tuple(mac, name)].
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+_dirname = os.path.abspath(os.path.dirname(__file__))
+README_PATH = os.path.join(_dirname, 'README.md')
+
+description = '''\
+Parses ISC dhcpd leases file and returns dict[IP, tuple(mac, name)].'''
+
+if os.path.exists(README_PATH):
+    long_description = open(README_PATH).read()
+else:
+    long_description = description
+
+setup(name='py-lease-file-parser',
+    version='0.1',
+    description=description,
+    license='BSD',
+    url='https://github.com/jm1024/LeaseInfo',
+    author='jm1024',
+    author_email='info@vxk.cz',
+    packages=find_packages(),
+    install_requires=[],
+    keywords="isc linux dhcpd lease parse",
+    include_package_data=True,
+)
