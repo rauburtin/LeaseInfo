8816121989213859928584a24117d779681101e1
Vaclav Klecanda <vencax77@gmail.com>
2014-06-10 20:22:51 +0200

    parsing stream instead of opening&closing file within main proc the printing code put into __main__ to prevent run on import

--- LeaseInfo.py
+++ LeaseInfo.py
@@ -6,47 +6,47 @@ def split_ip(ip):
 def ip_key(item):
     return split_ip(item[0])
 
-def parse(fileName):
+def parse(instream):
 
-    f = open( fileName, "r" )
     output = {}
     thisIp = ""
     thisMAC = ""
     thisName = ""
-    
-    for line in f:
-        
+
+    for line in instream:
+
         # ip
         if (line.find("lease") == 0):
             if (thisIp != ""):
                 output[thisIp] = thisIp + " " + thisMAC + " " + thisName
                 thisMAC = ""
                 thisName = ""
-                
+
             thisIp = line[line.find(" ") +1 : line.find("{") -1]
 
         # mac
         if(line.find("hardware ethernet") > -1):
             thisMAC = line[line.find("ethernet") +9 : line.find(";")]
-        
+
         # uid - will be replaced with name if it exists
         if(line.find("uid") > -1):
             thisName = line[line.find("uid") +5 : line.find(";") -1]
-            
+
         # hostname
         if(line.find("client-hostname") > -1):
             thisName = line[line.find("hostname") +10 : line.find(";") -1]
-            
+
     #final line
     if (thisIp != ""):
         output[thisIp] = thisIp + " " + thisMAC + " " + thisName
 
-    #print output
-    for k, v in sorted(output.items(), key=ip_key):
-        print(v)
+    return output
 
-    # Close the file
-    f.close()
-    
+
 #parse("/home/james/Source/LeaseInfo/dhcpd.leases")
-parse("/var/lib/dhcp/dhcpd.leases")
+if __name__ == '__main__':
+    with open("/var/lib/dhcp/dhcpd.leases") as f:
+        output = parse(f)
+        #print output
+        for k, v in sorted(output.items(), key=ip_key):
+            print(v)
